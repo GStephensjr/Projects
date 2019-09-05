d70e8d4e5f0ff6516946671363e6df78a640a8a7
Greg Stephens <greg.d.stephensjr@gmail.com>
2019-09-05 07:43:33 -0400

    project update

--- Heirarchy Database GUI/ProjectAccountAuthentication/Program.cs
+++ Heirarchy Database GUI/ProjectAccountAuthentication/Program.cs
@@ -68,24 +68,61 @@ namespace PasswordHashing
         }
 
         private static void accountCreate()
+        {
+            Console.Write("Please enter a UserName: ");
+            username = Console.ReadLine();
+            if (Users.ContainsKey(username))
             {
-                Console.Write("Please enter a UserName: ");
-                username = Console.ReadLine();
-                if (Users.ContainsKey(username))
-                {
-                    Console.WriteLine("User already exists");
-                    accountCreate();
-                }
-                Console.Write("Please enter a Password: ");
-                password = HashDirectory.Hash(Console.ReadLine());
-                //Console.WriteLine($"UserName: {username} \n Password: {password}");
-                Users.Add(username, password);
+                Console.WriteLine("User already exists");
+                accountCreate();
             }
-
-            private static void Exit()
-            {
-                System.Environment.Exit(0);
-            }
-
+            Console.Write("Please enter a Password: ");
+            //password = HashDirectory.Hash(Console.ReadLine());
+            password = HashDirectory.Hash(ReadPassword());
+            //Console.WriteLine($"UserName: {username} \n Password: {password}");
+            Users.Add(username, password);
         }
+
+        private static void Exit()
+        {
+            System.Environment.Exit(0);
+        }
+        public static string ReadPassword()
+        {
+            string password = "";
+            ConsoleKeyInfo info = Console.ReadKey(true);
+            while (info.Key != ConsoleKey.Enter)
+            {
+                if (info.Key != ConsoleKey.Backspace)
+                {
+                    Console.Write("*");
+                    password += info.KeyChar;
+                }
+                else if (info.Key == ConsoleKey.Backspace)
+                {
+                    if (!string.IsNullOrEmpty(password))
+                    {
+                        // remove one character from the list of password characters
+                        password = password.Substring(0, password.Length - 1);
+                        // get the location of the cursor
+                        int pos = Console.CursorLeft;
+                        // move the cursor to the left by one character
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                        // replace it with space
+                        Console.Write(" ");
+                        // move the cursor to the left by one character again
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                    }
+                }
+                info = Console.ReadKey(true);
+            }
+
+            // add a new line because user pressed enter at the end of their password
+            Console.WriteLine();
+            return password;
+        }
+
     }
+}
+    
+
